5c8380e44ccc6e51b875cd16c2bc47e4df65d61d
isac <isacskoglund02@gmail.com>
2023-04-19 10:57:10 +0200

    Setup project. Define hexarch api and model. Might change.

---
+++ src/hexarch/domain/models/product.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Optional
+from decimal import Decimal
+from identifiers import ProductId, ProductVersionId
+
+
+@dataclass(frozen=True)
+class Price:
+    amount: Decimal
+    unit: str
+    currency: str
+
+
+@dataclass(frozen=True)
+class Description:
+    pass
+
+
+@dataclass(frozen=True)
+class Name:
+    pass
+
+
+@dataclass(frozen=True)
+class ProductVersion:
+    id: ProductVersionId
+    name: Name
+    description: Description
+    price: Price
+
+
+@dataclass(frozen=True)
+class Product:
+    id: ProductId
+    current_version: Optional[ProductVersion] = None
