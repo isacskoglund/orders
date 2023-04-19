512aa4373bc2788930d312400a1c83d87fa4634d
isac <isacskoglund02@gmail.com>
2023-04-19 12:37:21 +0200

    Separate api and spi ports. Restructure namespace.

---
+++ src/hexarch/domain/ports/spi/add_product_version_port.py
@@ -0,0 +1,27 @@
+from typing import Protocol
+from dataclasses import dataclass
+from models.product import ProductVersion
+
+
+class AddProductVersionIdPort(Protocol):
+    # Result
+
+    @dataclass(frozen=True)
+    class AddProductVersionResult:
+        pass
+
+    class ProductVersionAlreadyExitsResult(AddProductVersionResult):
+        pass
+
+    class ProductNotFoundById(AddProductVersionResult):
+        pass
+
+    class SuccessfullyAddedProductVersionResult(AddProductVersionResult):
+        pass
+
+    # Port
+
+    def add_product_version(
+        self, product_version: ProductVersion
+    ) -> AddProductVersionResult:
+        raise NotImplementedError
