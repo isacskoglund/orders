5f46b198807d04d249b9d98cd4a669cfd141bc79
isac <isacskoglund02@gmail.com>
2023-04-19 13:05:51 +0200

    Correct previous commit. Separate api and spi ports. Restructure namespace.

--- src/hexarch/domain/ports/api/place_order_use_case.py
+++ src/hexarch/domain/ports/api/place_order_use_case.py
@@ -3,40 +3,34 @@ from dataclasses import dataclass
 from models.identifiers import CustomerId, ProductId
 from models.order import Order
 
-# Commands
-
-
-@dataclass(frozen=True)
-class PlaceOrderCommand:
-    @dataclass(frozen=True)
-    class CommandItem:
-        product_id: ProductId
-        quantity: int
-
-    customer_id: CustomerId
-    items: list[CommandItem]
-
-
-# Results
-
-
-@dataclass(frozen=True)
-class PlaceOrderResult:
-    pass
-
-
-class InvalidProductResult(PlaceOrderResult):
-    product_id: ProductId
-
-
-class SuccessfullyPlacedOrderResult(PlaceOrderResult):
-    new_user_was_created: bool
-    order: Order
-
-
-# Use Case
-
 
 class PlaceOrderUseCase(Protocol):
+    # Commands
+
+    @dataclass(frozen=True)
+    class PlaceOrderCommand:
+        @dataclass(frozen=True)
+        class CommandItem:
+            product_id: ProductId
+            quantity: int
+
+        customer_id: CustomerId
+        items: list[CommandItem]
+
+    # Results
+
+    @dataclass(frozen=True)
+    class PlaceOrderResult:
+        pass
+
+    class InvalidProductResult(PlaceOrderResult):
+        product_id: ProductId
+
+    class SuccessfullyPlacedOrderResult(PlaceOrderResult):
+        new_user_was_created: bool
+        order: Order
+
+    # Use Case
+
     def place_order(self, command: PlaceOrderCommand) -> PlaceOrderResult:
         raise NotImplementedError
