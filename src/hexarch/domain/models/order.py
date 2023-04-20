d52472d203428018f5ff5c16a3fb6bb8db6a4388
isac <isacskoglund02@gmail.com>
2023-04-20 16:16:34 +0200

    Remove all ports, clear all services implementations. Create a new order model.

--- src/hexarch/domain/models/order.py
+++ src/hexarch/domain/models/order.py
@@ -1,41 +1,72 @@
 from __future__ import annotations
-from identifiers import OrderId
+from identifiers import OrderId, ProductId, ProductVersionId
 from identifiers import CustomerId
 from enum import Enum
 from dataclasses import dataclass, replace
 from product import ProductVersion
 
 
-class OrderStatus(Enum):
-    PENDING = "pending"
-    PAYMENT_COMPLETE = "payment_complete"
-    SHIPPED = "shipped"
-    DELIVERED = "delivered"
-    CANCELLED = "cancelled"
-
-
 @dataclass(frozen=True)
 class Address:
-    street: str
-    city: str
-    postal_code: str
-    country: str
+    pass
 
 
 @dataclass(frozen=True)
-class OrderItem:
-    product_version: ProductVersion
-    quantity: int
+class RequestedOrder:
+    @dataclass(frozen=True)
+    class Item:
+        product_id: ProductId
+        quantity: int
 
-
-@dataclass(frozen=True)
-class OrderPreSave:
     customer_id: CustomerId
-    address: Address
-    items: list[OrderItem]
-    status: OrderStatus = OrderStatus.PENDING
+    shipping_address: Address
+    items: list[Item]
+
+    def to_versioned_order(
+        self, product_versions: dict[ProductId, ProductVersionId]
+    ) -> VersionedOrder:
+        items = [
+            VersionedOrder.Item(
+                product_id=item.product_id,
+                quantity=item.quantity,
+                product_version_id=product_versions[ProductId],
+            )
+            for item in self.items
+        ]
+        return VersionedOrder(
+            customer_id=self.customer_id,
+            shipping_address=self.shipping_address,
+            items=items,
+        )
 
 
-@dataclass(frozen=True)
-class Order(OrderPreSave):
-    id: OrderId
+class VersionedOrder(RequestedOrder):
+    class Item(RequestedOrder.Item):
+        product_version_id: ProductVersionId
+
+    def to_persisted_order(self, order_id: OrderId, status: PersistedOrder = None):
+        args = {
+            "customer_id": self.customer_id,
+            "shipping_address": self.shipping_address,
+            "items": self.items,
+            "order_id": order_id,
+        }
+        if status is not None:
+            args["status"] = status
+
+        return PersistedOrder(**args)
+
+
+class PersistedOrder(VersionedOrder):
+    class Status(Enum):
+        PENDING = "pending"
+        PAYMENT_COMPLETE = "payment_complete"
+        SHIPPED = "shipped"
+        DELIVERED = "delivered"
+        CANCELLED = "cancelled"
+
+    order_id: OrderId
+    status: Status = Status.PENDING
+
+    def update_status(self, new_status: Status) -> PersistedOrder:
+        return replace(self, status=new_status)
