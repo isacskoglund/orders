d9cf86d635d21b1ff8a9ba1b9da4a3946c51a6b8
isac <isacskoglund02@gmail.com>
2023-05-03 22:33:11 +0200

    Split order persistence spi port into multiple classes for lighter implementation.

--- src/domain/ports/spi/order_persistence_spi.py
+++ src/domain/ports/spi/order_persistence_spi.py
@@ -3,7 +3,7 @@ from domain.models.order import VersionedOrder, PersistedOrder
 from domain.models.identifier import Identifier
 
 
-class OrderPersistenceSPI(Protocol):
+class GetOrderSPI(Protocol):
     def get_orders_by_order_ids(
         self, order_ids: list[Identifier]
     ) -> list[PersistedOrder]:
@@ -14,9 +14,13 @@ class OrderPersistenceSPI(Protocol):
     ) -> list[PersistedOrder]:
         raise NotImplementedError
 
+
+class SaveOrderSPI(Protocol):
     def save_order(self, versioned_order: VersionedOrder) -> PersistedOrder:
         raise NotImplementedError
 
+
+class UpdateOrderSPI(Protocol):
     def update_order_status(
         self, order_id: Identifier, new_status: PersistedOrder.Status
     ) -> None:
