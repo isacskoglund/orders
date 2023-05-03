752749fe8b6ac7364cac079eb403401320f548c6
isac <isacskoglund02@gmail.com>
2023-05-03 15:41:06 +0200

    Add test for StatusToEventMapper.

---
+++ tests/test_domain/test_models/test_dispatchable_event.py
@@ -0,0 +1,23 @@
+from typing import Callable
+from domain.models.order import PersistedOrder
+from domain.models.event import StatusToEventMapper
+
+
+def test_map_status_to_event() -> None:
+    mapper = StatusToEventMapper()
+    for status in PersistedOrder.Status:
+        expected_result = None
+        if status in mapper.status_to_event_map:
+            expected_result = mapper.status_to_event_map[status]
+        assert expected_result == mapper.map_status_to_event(status=status)
+
+
+def test_create_event(persisted_order: PersistedOrder) -> None:
+    mapper = StatusToEventMapper()
+    for status in PersistedOrder.Status:
+        updated_order = persisted_order.update_status(new_status=status)
+        event = mapper.create_event(order=updated_order)
+        if status in mapper.status_to_event_map:
+            assert isinstance(event, mapper.status_to_event_map[status])
+        else:
+            assert event is None
