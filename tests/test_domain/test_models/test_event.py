e44ef6497c3e888f90bd29b41444aef0af160e32
isac <isacskoglund02@gmail.com>
2023-05-09 19:45:14 +0200

    Make status to event mapper singleton.

--- tests/test_domain/test_models/test_event.py
+++ tests/test_domain/test_models/test_event.py
@@ -7,8 +7,8 @@ def test_map_status_to_event() -> None:
     mapper = StatusToEventMapper()
     for status in Status:
         expected_result = None
-        if status in mapper.status_to_event_map:
-            expected_result = mapper.status_to_event_map[status]
+        if status in StatusToEventMapper.status_to_event_map:
+            expected_result = StatusToEventMapper.status_to_event_map[status]
         assert expected_result == mapper.map_status_to_event(status=status)
 
 
@@ -17,7 +17,14 @@ def test_create_event(persisted_order: PersistedOrder) -> None:
     for status in Status:
         updated_order = persisted_order.update_status(new_status=status)
         event = mapper.create_event(order=updated_order)
-        if status in mapper.status_to_event_map:
-            assert isinstance(event, mapper.status_to_event_map[status])
+        if status in StatusToEventMapper.status_to_event_map:
+            assert isinstance(event, StatusToEventMapper.status_to_event_map[status])
         else:
             assert event is None
+
+
+def test_mapper_is_singleton() -> None:
+    mapper1 = StatusToEventMapper()
+    mapper2 = StatusToEventMapper()
+    assert isinstance(mapper1, StatusToEventMapper)
+    assert mapper1 is mapper2
