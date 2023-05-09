c2d97183b44dd70ecc335a1943c91c600fb6087f
isac <isacskoglund02@gmail.com>
2023-05-09 14:03:11 +0200

    Add place order service with complete testing. Add test-classes to conftest.py in test_services to be reused by other tests.

--- src/domain/ports/spi/product_catalogue_spi.py
+++ src/domain/ports/spi/product_catalogue_spi.py
@@ -1,16 +1,52 @@
 from typing import Protocol
 from domain.models.identifier import Identifier
 from domain.models.product import Product, ProductVersion
+from dataclasses import dataclass
 
 
-class ProductCatalogueSPI(Protocol):
-    def validate_product_id(self, product_id: Identifier) -> bool:
-        pass
+class ValidateProductIdSPI(Protocol):
+    @dataclass
+    class Result:
+        valid_ids: set[Identifier]
+        invalid_ids: set[Identifier]
 
-    def get_product(self, product_id: Identifier) -> Product | None:
-        pass
+    def validate_product_ids(self, product_ids: list[Identifier]) -> Result:
+        ...
 
-    def get_product_version(
-        self, product_version_id: Identifier
-    ) -> ProductVersion | None:
-        pass
+
+class GetProductSPI(Protocol):
+    @dataclass
+    class Result:
+        products: dict[Identifier, Product]
+        invalid_ids: set[Identifier]
+
+    def get_product(self, product_ids: list[Identifier]) -> Result:
+        ...
+
+
+class GetProductVersionIdsSPI(Protocol):
+    @dataclass
+    class Result:
+        product_version_ids: dict[Identifier, Identifier]
+        invalid_ids: set[Identifier]
+        ids_without_product_version_id: set[
+            Identifier
+        ]  # Contains each valid id in product_ids that
+        # have no product_version_id currently assigned to its product.
+
+    def get_product_versions(self, product_ids: list[Identifier]) -> Result:
+        ...
+
+
+class GetProductVersionsSPI(Protocol):
+    @dataclass
+    class Result:
+        product_versions: dict[Identifier, ProductVersion]
+        invalid_ids: set[Identifier]
+        ids_without_product_version_id: set[
+            Identifier
+        ]  # Contains each valid id in product_ids that
+        # have no product_version currently assigned to its product.
+
+    def get_product_versions(self, product_ids: list[Identifier]) -> Result:
+        ...
