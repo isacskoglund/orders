f170a9a6ca582f2cf494683b47ee831a0319f107
isac <isacskoglund02@gmail.com>
2023-05-09 14:03:11 +0200

    Add place order service with complete testing. Add test-classes to conftest.py in test_services to be reused by other tests.

---
+++ tests/test_domain/test_services/conftest.py
@@ -0,0 +1,148 @@
+from __future__ import annotations
+from domain.ports.spi.order_persistence_spi import (
+    GetOrderByOrderIdSPI,
+    UpdateOrderSPI,
+    SaveOrderSPI,
+)
+from domain.ports.spi.product_catalogue_spi import GetProductVersionIdsSPI
+from domain.ports.spi.status_update_event_dispatcher_spi import (
+    StatusUpdateEventDispatcherSPI,
+)
+from domain.models.identifier import Identifier
+from domain.models.order_status import Status
+from domain.models.order import PersistedOrder, VersionedOrder
+from domain.models.event import DispatchableEvent
+from typing import Callable
+from pytest import fixture
+from dataclasses import dataclass, field
+
+
+class UpdateOrderDummy(UpdateOrderSPI):
+    statuses: dict[Identifier, Status] = {}
+
+    def update_order_status(self, order_id: Identifier, new_status: Status) -> None:
+        self.statuses[order_id] = new_status
+
+    def reset(self) -> None:
+        self.statuses = {}
+
+    def read(self) -> dict[Identifier, Status]:
+        return self.statuses
+
+    def is_empty(self) -> bool:
+        return self.statuses == {}
+
+
+@dataclass
+class SaveOrderDummy(SaveOrderSPI):
+    id_generator: Callable[[], Identifier]
+
+    orders: dict[Identifier, PersistedOrder] = field(default_factory=dict)
+
+    def save_order(self, versioned_order: VersionedOrder) -> PersistedOrder:
+        persisted_order = versioned_order.to_persisted_order(id=id)
+        self.orders[id] = persisted_order
+        return persisted_order
+
+    def reset(self) -> None:
+        self.orders = {}
+
+    def read(self) -> dict[Identifier, PersistedOrder]:
+        return self.orders
+
+    def is_empty(self) -> bool:
+        return self.orders == {}
+
+
+class GetProductVersionIdsDummy(GetProductVersionIdsSPI):
+    product_version_ids: dict[Identifier, Identifier | None] = {}
+
+    def get_product_versions(
+        self, product_ids: list[Identifier]
+    ) -> GetProductVersionIdsSPI.Result:
+        result = GetProductVersionIdsSPI.Result(
+            product_version_ids={},
+            invalid_ids=set(),
+            ids_without_product_version_id=set(),
+        )
+        for id in product_ids:
+            if id not in self.product_version_ids:
+                result.invalid_ids.add(id)
+                continue
+            if self.product_version_ids[id] is None:
+                result.ids_without_product_version_id.add(id)
+                continue
+            product_version_id = self.product_version_ids[id]
+            result.product_version_ids[id] = product_version_id
+        return result
+
+    def reset(self) -> None:
+        self.product_version_ids = {}
+
+    def set(self, product_version_ids: dict[Identifier, Identifier | None]) -> None:
+        self.product_version_ids = product_version_ids
+
+    def read(self) -> dict[Identifier, Identifier | None]:
+        return self.product_version_ids
+
+    def is_empty(self) -> bool:
+        return self.product_version_ids == {}
+
+    def add(
+        self, product_id: Identifier, product_version_id: Identifier | None
+    ) -> None:
+        self.product_version_ids[product_id] = product_version_id
+
+
+class GetOrderByOrderIdDummy(GetOrderByOrderIdSPI):
+    orders: dict[Identifier, PersistedOrder] = {}
+
+    def get_order_by_order_id(self, order_id: Identifier) -> PersistedOrder | None:
+        return self.orders.get(order_id)
+
+    def reset(self) -> None:
+        self.orders = {}
+
+    def add(self, order: PersistedOrder) -> None:
+        self.orders[order.id] = order
+
+
+class EventDispatcherDummy(StatusUpdateEventDispatcherSPI):
+    dispatched_events: list[DispatchableEvent] = []
+
+    def dispatch_event(self, event: DispatchableEvent) -> None:
+        self.dispatched_events.append(event)
+
+    def reset(self) -> None:
+        self.dispatched_events = []
+
+    def read(self) -> list[DispatchableEvent]:
+        return self.dispatched_events
+
+    def is_empty(self) -> bool:
+        return self.dispatched_events == []
+
+
+@fixture
+def update_order_dummy() -> UpdateOrderDummy:
+    return UpdateOrderDummy
+
+
+@fixture
+def save_order_dummy(id_generator) -> SaveOrderDummy:
+    return SaveOrderDummy(id_generator=id_generator)
+
+
+@fixture
+def get_product_version_ids_dummy() -> GetProductVersionIdsDummy:
+    return GetProductVersionIdsDummy()
+
+
+@fixture
+def get_order_by_id_dummy() -> GetOrderByOrderIdDummy:
+    return GetOrderByOrderIdDummy()
+
+
+@fixture
+def event_dispatcher_dummy() -> EventDispatcherDummy:
+    return EventDispatcherDummy()
---
+++ tests/test_domain/test_services/test_place_order_service.py
@@ -0,0 +1,194 @@
+from domain.services.place_order_service import PlaceOrderService
+from domain.models.identifier import Identifier
+from domain.models.order import RequestedOrder, VersionedOrder, PersistedOrder
+from domain.errors import InvalidProductIdError, NoCurrentProductVersionError
+from conftest import (
+    SaveOrderDummy,
+    GetProductVersionIdsDummy,
+    EventDispatcherDummy,
+)
+from typing import Callable
+from random import randint
+from pytest import fixture
+import pytest
+
+MAX_ORDER_ITEM_QUANTITY = 10
+
+# Product Ids:
+
+
+@fixture
+def product_ids_to_version_ids(
+    product_versions: dict[Identifier, Identifier]
+) -> dict[Identifier, Identifier]:
+    return product_versions
+
+
+@fixture
+def invalid_product_id(id_generator: Callable[[], Identifier]) -> Identifier:
+    return id_generator()
+
+
+@fixture
+def valid_product_id_with_missing_product_version(
+    id_generator: Callable[[], Identifier]
+) -> Identifier:
+    return id_generator()
+
+
+# Order Items:
+
+
+@fixture
+def valid_items(
+    product_ids_to_version_ids: dict[Identifier, Identifier]
+) -> list[RequestedOrder.Item]:
+    return [
+        RequestedOrder.Item(
+            product_id=product_id, quantity=randint(0, MAX_ORDER_ITEM_QUANTITY)
+        )
+        for product_id in product_ids_to_version_ids
+    ]
+
+
+@fixture
+def invalid_item(invalid_product_id) -> RequestedOrder.Item:
+    return RequestedOrder.Item(
+        product_id=invalid_product_id, quantity=randint(0, MAX_ORDER_ITEM_QUANTITY)
+    )
+
+
+@fixture
+def valid_item_with_missing_product_version(
+    valid_product_id_with_missing_product_version,
+) -> RequestedOrder.Item:
+    return RequestedOrder.Item(
+        product_id=valid_product_id_with_missing_product_version,
+        quantity=randint(0, MAX_ORDER_ITEM_QUANTITY),
+    )
+
+
+# Orders:
+
+
+@fixture
+def valid_order(valid_items, id_generator, address_generator) -> RequestedOrder:
+    return RequestedOrder(
+        customer_id=id_generator(),
+        shipping_address=address_generator(),
+        items=valid_items,
+    )
+
+
+@fixture
+def invalid_order(
+    valid_items, invalid_item, id_generator, address_generator
+) -> RequestedOrder:
+    return RequestedOrder(
+        customer_id=id_generator(),
+        shipping_address=address_generator(),
+        items=[*valid_items, invalid_item],
+    )
+
+
+@fixture
+def valid_order_with_missing_product_version(
+    valid_items,
+    valid_item_with_missing_product_version,
+    id_generator,
+    address_generator,
+) -> RequestedOrder:
+    return RequestedOrder(
+        customer_id=id_generator(),
+        shipping_address=address_generator(),
+        items=[*valid_items, valid_item_with_missing_product_version],
+    )
+
+
+@fixture
+def valid_versioned_order(
+    valid_order: RequestedOrder, product_ids_to_version_ids
+) -> VersionedOrder:
+    return valid_order.to_versioned_order(product_versions=product_ids_to_version_ids)
+
+
+# Tests:
+
+
+def test_place_order_service_invalid_product_id(
+    get_product_version_ids_dummy: GetProductVersionIdsDummy,
+    save_order_dummy: SaveOrderDummy,
+    event_dispatcher_dummy: EventDispatcherDummy,
+    product_ids_to_version_ids: dict[Identifier, Identifier],
+    invalid_product_id: Identifier,
+    invalid_order: RequestedOrder,
+) -> None:
+    service = PlaceOrderService(
+        get_product_version_ids_spi=get_product_version_ids_dummy,
+        save_order_spi=save_order_dummy,
+        event_dispatcher=event_dispatcher_dummy,
+    )
+    get_product_version_ids_dummy.set(product_version_ids=product_ids_to_version_ids)
+    with pytest.raises(InvalidProductIdError) as error_info:
+        service.place_order(requested_order=invalid_order)
+    assert error_info.value.product_id == invalid_product_id
+    assert save_order_dummy.is_empty()
+    assert event_dispatcher_dummy.is_empty()
+
+
+def test_place_order_service_valid_id_without_version(
+    get_product_version_ids_dummy: GetProductVersionIdsDummy,
+    save_order_dummy: SaveOrderDummy,
+    event_dispatcher_dummy: EventDispatcherDummy,
+    product_ids_to_version_ids: dict[Identifier, Identifier],
+    valid_product_id_with_missing_product_version: Identifier,
+    valid_order_with_missing_product_version: RequestedOrder,
+) -> None:
+    service = PlaceOrderService(
+        get_product_version_ids_spi=get_product_version_ids_dummy,
+        save_order_spi=save_order_dummy,
+        event_dispatcher=event_dispatcher_dummy,
+    )
+    get_product_version_ids_dummy.set(
+        product_version_ids={
+            **product_ids_to_version_ids,
+            valid_product_id_with_missing_product_version: None,
+        }
+    )
+    with pytest.raises(NoCurrentProductVersionError) as error_info:
+        service.place_order(requested_order=valid_order_with_missing_product_version)
+    assert error_info.value.product_id == valid_product_id_with_missing_product_version
+    assert save_order_dummy.is_empty()
+    assert event_dispatcher_dummy.is_empty()
+
+
+def test_place_order_service_success(
+    get_product_version_ids_dummy: GetProductVersionIdsDummy,
+    save_order_dummy: SaveOrderDummy,
+    event_dispatcher_dummy: EventDispatcherDummy,
+    product_ids_to_version_ids: dict[Identifier, Identifier],
+    valid_order: RequestedOrder,
+    valid_versioned_order: VersionedOrder,
+) -> None:
+    service = PlaceOrderService(
+        get_product_version_ids_spi=get_product_version_ids_dummy,
+        save_order_spi=save_order_dummy,
+        event_dispatcher=event_dispatcher_dummy,
+    )
+    get_product_version_ids_dummy.set(product_version_ids=product_ids_to_version_ids)
+    save_order_dummy.reset()
+    event_dispatcher_dummy.reset()
+
+    service.place_order(requested_order=valid_order)
+
+    saved_orders = save_order_dummy.read()
+    assert len(saved_orders) == 1
+    expected_order_id = next(iter(saved_orders))
+    expected_persisted_order = valid_versioned_order.to_persisted_order(
+        id=expected_order_id
+    )
+    assert expected_persisted_order == saved_orders[expected_order_id]
+
+    dispatched_events = event_dispatcher_dummy.read()
+    assert len(dispatched_events) == 1
+    assert dispatched_events[0].order == expected_persisted_order
