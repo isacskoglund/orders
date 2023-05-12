5bd66f2799d7c9f6cbab7ade39256d222b6eba80
isac <isacskoglund02@gmail.com>
2023-05-12 21:10:56 +0200

    Add fixtures and dataclass to extract setup from testcases.

--- tests/test_domain/test_services/test_place_order_service.py
+++ tests/test_domain/test_services/test_place_order_service.py
@@ -14,48 +14,70 @@ from test_domain.dummies import (
     StatusToEventMapperDummy,
 )
 from typing import Callable
+from dataclasses import dataclass
 import pytest
 
+
 # Tests:
+@dataclass
+class Dummies:
+    get_product_version_ids_dummy: GetProductVersionIdsDummy
+    save_order_dummy: SaveOrderDummy
+    event_dispatcher_dummy: EventDispatcherDummy
+    status_to_event_mapper_dummy: StatusToEventMapperDummy
+
+
+@pytest.fixture
+def dummies(
+    product_version_ids: dict[Identifier, Identifier], persisted_order: PersistedOrder
+) -> Dummies:
+    dummies = Dummies(
+        get_product_version_ids_dummy=GetProductVersionIdsDummy(
+            product_version_ids=product_version_ids
+        ),
+        save_order_dummy=SaveOrderDummy(persisted_order_to_return=persisted_order),
+        event_dispatcher_dummy=EventDispatcherDummy(),
+        status_to_event_mapper_dummy=StatusToEventMapperDummy(),
+    )
+    return dummies
+
+
+@pytest.fixture
+def service(dummies: Dummies) -> PlaceOrderService:
+    return PlaceOrderService(
+        get_product_version_ids_spi=dummies.get_product_version_ids_dummy,
+        save_order_spi=dummies.save_order_dummy,
+        event_dispatcher=dummies.event_dispatcher_dummy,
+        _event_mapper=dummies.status_to_event_mapper_dummy,
+    )
 
 
 def test_place_order_service_raises(
-    product_version_ids: dict[Identifier, Identifier],
     id_generator: Callable[[], Identifier],
     requested_order: RequestedOrder,
-    persisted_order: PersistedOrder,
+    dummies: Dummies,
+    service: PlaceOrderService,
 ) -> None:
-    get_product_version_ids_dummy = GetProductVersionIdsDummy(
-        product_version_ids=product_version_ids
-    )
-    save_order_dummy = SaveOrderDummy(persisted_order_to_return=persisted_order)
-    event_dispatcher_dummy = EventDispatcherDummy()
-    service = PlaceOrderService(
-        get_product_version_ids_spi=get_product_version_ids_dummy,
-        save_order_spi=save_order_dummy,
-        event_dispatcher=event_dispatcher_dummy,
-    )
-
     # Assert raises `InvalidProductIdError` on `invalid_ids` not empty:
     invalid_product_id = id_generator()
-    get_product_version_ids_dummy.invalid_ids = {invalid_product_id}
+    dummies.get_product_version_ids_dummy.invalid_ids = {invalid_product_id}
     with pytest.raises(InvalidProductIdError) as error_info:
         service.place_order(requested_order=requested_order)
     assert error_info.value.product_id == invalid_product_id
-    assert save_order_dummy.is_empty()
-    assert event_dispatcher_dummy.is_empty()
-    get_product_version_ids_dummy.invalid_ids = set()
+    assert dummies.save_order_dummy.is_empty()
+    assert dummies.event_dispatcher_dummy.is_empty()
+    dummies.get_product_version_ids_dummy.invalid_ids = set()
 
     # Assert raises `NoCurrentProductVersionError` on `ids_without_product_version_id` not empty:
     product_id_without_product_version_id = id_generator()
-    get_product_version_ids_dummy.ids_without_product_version_id = {
+    dummies.get_product_version_ids_dummy.ids_without_product_version_id = {
         product_id_without_product_version_id
     }
     with pytest.raises(NoCurrentProductVersionError) as error_info:
         service.place_order(requested_order=requested_order)
     assert error_info.value.product_id == product_id_without_product_version_id
-    assert save_order_dummy.is_empty()
-    assert event_dispatcher_dummy.is_empty()
+    assert dummies.save_order_dummy.is_empty()
+    assert dummies.event_dispatcher_dummy.is_empty()
 
 
 def test_place_order_service_success(
@@ -63,28 +85,19 @@ def test_place_order_service_success(
     requested_order: RequestedOrder,
     versioned_order: VersionedOrder,
     persisted_order: PersistedOrder,
+    dummies: Dummies,
+    service: PlaceOrderService,
 ) -> None:
-    get_product_version_ids_dummy = GetProductVersionIdsDummy()
-    save_order_dummy = SaveOrderDummy(persisted_order_to_return=persisted_order)
-    event_dispatcher_dummy = EventDispatcherDummy()
-    status_to_event_mapper_dummy = StatusToEventMapperDummy()
-    service = PlaceOrderService(
-        get_product_version_ids_spi=get_product_version_ids_dummy,
-        save_order_spi=save_order_dummy,
-        event_dispatcher=event_dispatcher_dummy,
-        _event_mapper=status_to_event_mapper_dummy,
-    )
-
     # Setup:
     event_type = DispatchableEvent.EventType.CANCELLED
     expected_event = DispatchableEvent(order=persisted_order, event_type=event_type)
-    status_to_event_mapper_dummy.event_type = event_type
-    get_product_version_ids_dummy.product_version_ids = product_version_ids
+    dummies.status_to_event_mapper_dummy.event_type = event_type
+    dummies.get_product_version_ids_dummy.product_version_ids = product_version_ids
 
     # Run:
     persisted_order_result = service.place_order(requested_order=requested_order)
 
     # Asserts:
     assert persisted_order_result == persisted_order
-    assert save_order_dummy.read() == [versioned_order]
-    assert event_dispatcher_dummy.read() == [expected_event]
+    assert dummies.save_order_dummy.read() == [versioned_order]
+    assert dummies.event_dispatcher_dummy.read() == [expected_event]
