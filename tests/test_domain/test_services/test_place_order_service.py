0ac4a42501dd1b629ebbe1d250fe6300f63d9a1a
isac <isacskoglund02@gmail.com>
2023-05-12 19:46:57 +0200

    Merge similar test cases. Remove unnecessary dependencies on fixtures.

--- tests/test_domain/test_services/test_place_order_service.py
+++ tests/test_domain/test_services/test_place_order_service.py
@@ -128,13 +128,15 @@ def valid_versioned_order(
 # Tests:
 
 
-def test_place_order_service_invalid_product_id(
-    product_ids_to_version_ids: dict[Identifier, Identifier],
-    invalid_product_id: Identifier,
-    invalid_order: RequestedOrder,
+def test_place_order_service_raises(
+    product_version_ids: dict[Identifier, Identifier],
+    id_generator: Callable[[], Identifier],
+    requested_order: RequestedOrder,
     persisted_order: PersistedOrder,
 ) -> None:
-    get_product_version_ids_dummy = GetProductVersionIdsDummy()
+    get_product_version_ids_dummy = GetProductVersionIdsDummy(
+        product_version_ids=product_version_ids
+    )
     save_order_dummy = SaveOrderDummy(persisted_order_to_return=persisted_order)
     event_dispatcher_dummy = EventDispatcherDummy()
     service = PlaceOrderService(
@@ -143,46 +145,24 @@ def test_place_order_service_invalid_product_id(
         event_dispatcher=event_dispatcher_dummy,
     )
 
-    # Setup:
-    get_product_version_ids_dummy.product_version_ids = product_ids_to_version_ids
+    # Assert raises `InvalidProductIdError` on `invalid_ids` not empty:
+    invalid_product_id = id_generator()
     get_product_version_ids_dummy.invalid_ids = {invalid_product_id}
-
-    # Run:
     with pytest.raises(InvalidProductIdError) as error_info:
-        service.place_order(requested_order=invalid_order)
-
-    # Asserts:
+        service.place_order(requested_order=requested_order)
     assert error_info.value.product_id == invalid_product_id
     assert save_order_dummy.is_empty()
     assert event_dispatcher_dummy.is_empty()
+    get_product_version_ids_dummy.invalid_ids = set()
 
-
-def test_place_order_service_valid_id_without_version(
-    product_ids_to_version_ids: dict[Identifier, Identifier],
-    valid_product_id_with_missing_product_version: Identifier,
-    valid_order_with_missing_product_version: RequestedOrder,
-    persisted_order: PersistedOrder,
-) -> None:
-    get_product_version_ids_dummy = GetProductVersionIdsDummy()
-    save_order_dummy = SaveOrderDummy(persisted_order_to_return=persisted_order)
-    event_dispatcher_dummy = EventDispatcherDummy()
-    service = PlaceOrderService(
-        get_product_version_ids_spi=get_product_version_ids_dummy,
-        save_order_spi=save_order_dummy,
-        event_dispatcher=event_dispatcher_dummy,
-    )
-    # Setup
-    get_product_version_ids_dummy.product_version_ids = product_ids_to_version_ids
+    # Assert raises `NoCurrentProductVersionError` on `ids_without_product_version_id` not empty:
+    product_id_without_product_version_id = id_generator()
     get_product_version_ids_dummy.ids_without_product_version_id = {
-        valid_product_id_with_missing_product_version
+        product_id_without_product_version_id
     }
-
-    # Run:
     with pytest.raises(NoCurrentProductVersionError) as error_info:
-        service.place_order(requested_order=valid_order_with_missing_product_version)
-
-    # Asserts:
-    assert error_info.value.product_id == valid_product_id_with_missing_product_version
+        service.place_order(requested_order=requested_order)
+    assert error_info.value.product_id == product_id_without_product_version_id
     assert save_order_dummy.is_empty()
     assert event_dispatcher_dummy.is_empty()
 
