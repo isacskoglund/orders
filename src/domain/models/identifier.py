6456a2430bde0476019c40e9c9f884b7136a90f6
isac <isacskoglund02@gmail.com>
2023-05-27 14:29:20 +0200

    Turn Identifier into a Protocol.

--- src/domain/models/identifier.py
+++ src/domain/models/identifier.py
@@ -1,4 +1,7 @@
-class Identifier:
+from typing import Protocol
+
+
+class Identifier(Protocol):
     """
     Abstract identifier class.
     Implementation should make each instance unique to the object it represents.
