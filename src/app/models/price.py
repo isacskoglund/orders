2b92f977e626009bde88e2259b23022b5aa8c7f9
isac <isacskoglund02@gmail.com>
2023-05-28 16:15:53 +0200

    Improve Currency validation. Add tests.

--- src/app/models/price.py
+++ src/app/models/price.py
@@ -1,17 +1,29 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.exceptions import ValidationError
+
+
+def validate_currency_code(code: str):
+    if not code.isalpha():
+        raise ValidationError("Currency code must contain only alphabetic characters.")
+    if len(code) != 3:
+        raise ValidationError("Currency code must be exactly 3 characters long.")
+    if code.upper() != code:
+        raise ValidationError("Currency code must be in uppercase.")
 
 
 class Currency(models.Model):
     code = models.CharField(
         max_length=3,
         primary_key=True,
-        validators=[MinLengthValidator(3), MaxLengthValidator(3)],
+        validators=[validate_currency_code],
     )
     name = models.CharField(max_length=50)
 
     class Meta:  # type: ignore
         db_table = "currency"
+
+    def __repr__(self) -> str:
+        return f"Currency(name={self.name}, code={self.code})"
 
 
 class PriceABC(models.Model):
---
+++ tests/test_django_project/test_app/test_models/test_price.py
@@ -0,0 +1,31 @@
+from tests.test_django_project.factories import CurrencyFactory
+from app.models import Currency
+
+import pytest
+from django.core.exceptions import ValidationError
+
+
+@pytest.mark.parametrize(
+    "code",
+    ("aaaa", "AAAA", "Aa", "a", "", "aaa", "aAA"),
+)
+@pytest.mark.django_db
+def test_currency_code_invalid(
+    code: str,
+    currency_factory: CurrencyFactory,
+) -> None:
+    currency: Currency = currency_factory.build(code=code)
+
+    with pytest.raises(ValidationError):
+        currency.full_clean()
+
+
+@pytest.mark.parametrize("code", ("EEE", "JEK", "EUR"))
+@pytest.mark.django_db
+def test_currency_code_valid(code: str, currency_factory: CurrencyFactory) -> None:
+    currency: Currency = currency_factory.build(code=code)
+
+    try:
+        currency.full_clean()
+    except ValidationError as e:
+        assert False, f"Raised {e}."
